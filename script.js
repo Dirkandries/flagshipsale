37a983c91c3525ee5c04716fcb11e452c5f8fd74
LuukFYI <luuk@flagship.fyi>
2024-04-10 11:06:09 +0200

    Update script.js to include JSONify points

--- script.js
+++ script.js
@@ -143,6 +143,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.querySelector('.mask-modal').classList.add('active');
                 document.querySelector('.modal').classList.add('modal-active');
                 Telegram.WebApp.MainButton.show();
+                // Send the winning sector points to Telegram
+                Telegram.WebApp.sendData(JSON.stringify({ points: this.winningSector }));
 
                 Telegram.WebApp.MainButton.onClick(() => {
                     Telegram.WebApp.close();
